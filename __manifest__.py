575d69a5dd746e0a37370d9032dad8516bdfa5f1
raul16stv <raulmartinezzgz@gmail.com>
2018-02-27 11:31:56 +0100

    Modelo preparado, falta posible ampliacion y views
    Signed-off-by: raul16stv <raulmartinezzgz@gmail.com>

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "proyecto",
+
+    'summary': """
+        modulo de torneo maria auxiliadoria""",
+
+    'description': """
+        Modulo final
+    """,
+
+    'author': "Raul martinez",
+    'website': "https://zaragoza.salesianos.edu/",
+    'category': 'proyecto',
+    'version': '1.0',
+    'data': [
+        'views/base.xml',
+    ],
+}
---
+++ models.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models,fields
+
+class alumno(models.Model):
+    _name = 'proyecto.alumno'
+
+    nombre = fields.Char()
+    clase= fields.Many2one('proyecto.alumnos', string="Clase")
+ 
+class clase(models.Model):
+    _name = 'proyecto.clase'
+
+
+    nombre = fields.Char()
+    alumnos= fields.One2many('proyecto.alumno','clase',string="alumnos")
+    equipos= fields.Many2one('proyecto.equipo', string="equipos")
+class equipo(models.Model):
+    _name = 'proyecto.equipo'
+
+    nombre= fields.Char()
+    clase= fields.One2many('proyecto.clase','equipos',string="equipos")
+    deporte= fields.One2many('proyecto.deporte','equipos ',string="deportes")
+
+class deporte(models.Model):
+    _name = 'proyecto.deporte'
+
+    nombre= fields.Char()
+    equipos= fields.Many2one('proyecto.equipo', string="equipos")
+    partidos= fields.Many2one('proyecto.partido', string="partidos")
+
+  
+class partido(models.Model):
+    _name = 'proyecto.partido'
+
+    deporte = fields.One2many('proyecto.deporte','partidos', string="partidos")
+    
